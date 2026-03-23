ced83edb4da65346e0602c99be19bcd6ae4e2ed4
qiyue <repository@qiyue.com>
2026-03-23 16:45:49 +0800

    feat: init ddd project

---
+++ chatbot-api-infrastructure/src/main/java/cn/chatbot/api/App.java
@@ -0,0 +1,13 @@
+package cn.chatbot.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/chatbot/api/test/ApiTest.java
@@ -0,0 +1,7 @@
+package cn.chatbot.api.test;
+
+/**
+ * @description 启动入口
+ */
+public class ApiTest {
+}
